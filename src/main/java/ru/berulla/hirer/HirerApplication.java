f2b3f58be7b3eba60af7cc9e8763bd6f510bd1e8
VladimirSaberullin <saberullin.vld@yandex.ru>
2022-09-05 13:32:25 +0300

    Initial commit

---
+++ src/main/java/ru/berulla/hirer/HirerApplication.java
@@ -0,0 +1,13 @@
+package ru.berulla.hirer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/berulla/hirer/HirerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.berulla.hirer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
